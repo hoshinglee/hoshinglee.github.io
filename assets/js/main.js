0d8429f6e319e479ade1d89169a22d525b857fb9
hoshinglee <hoshinglee.g@gmail.com>
2023-11-07 00:27:22 +0800

    light

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-AOS.init();
-// You can also pass an optional settings object
-// below listed default settings
+// enable aos
 AOS.init({
-  
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
@@ -14,5 +11,90 @@ AOS.init({
 
 });
 
+// enable tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// document.querySelector('.theme-toggle-button').addEventListener(
+//   'click', () => {
+//     document.body.classList.toggle('dark')
+//   }
+// )
+
+// enable theme switching
+const themeicon = document.querySelectorAll('.theme-toggler-icon');
+const dropdown = document.querySelectorAll('.dropdown');
+const carousel = document.querySelectorAll('.carousel');
+const navbaricon = document.querySelectorAll('.navbar-toggler-icon');
+const logoicon = document.querySelectorAll('.logo');
+const gradio = document.querySelectorAll('.gradio');
+
+const themes = {
+  light: {
+    '--color-body': '#373d3e',
+    '--color-heading': '#171715',
+    '--color-base': '#ebebeb',
+    '--color-base2': '#ffffff',
+    // '--color-brand': '#42c3d9',
+    '--color-brand': '#5fcff6',
+    // '--color-brand': '#00a8ff',
+    // '--color-brand': '#007bff',
+    // '--color-brand': '#95fcfd',
+    '--color-brand2': '#000000',
+  },
+  dark: {
+    '--color-body': '#b6cbce',
+    '--color-heading': '#eef3db',
+    '--color-base': '#151515',
+    '--color-base2': '#0c0c0c',
+    '--color-brand': '#fef417',
+    '--color-brand2': '#ffffff',
+  },
+};
+
+[...document.querySelectorAll('.theme-toggler-button')].forEach(el => {
+  el.addEventListener('click', () => {
+      const currentTheme = document.documentElement.getAttribute('data-theme');
+      const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+      const theme = themes[newTheme];
+
+      // const theme = themes[el.dataset.theme];
+      for (var variable in theme) {
+          document.querySelector(":root").style.setProperty(variable, theme[variable]);
+      };
+      document.documentElement.setAttribute('data-theme', newTheme);
+
+      // Update additional elements with data-bs-theme attribute
+      // gradio.forEach(element => {
+      //   element.setAttribute('theme-mode', newTheme);
+      // });
+      dropdown.forEach(element => {
+        element.setAttribute('data-bs-theme', newTheme);
+      });
+      carousel.forEach(element => {
+        element.setAttribute('data-bs-theme', currentTheme);
+      });
+      navbaricon.forEach(element => {
+        element.classList.toggle('light');
+      });
+      themeicon.forEach(element => {
+        element.classList.toggle('light');
+        if(element.classList.contains('light')){
+          // element.src = "assets/images/day-mode.png";
+          element.classList.remove('la-star-and-crescent');
+          element.classList.add('la-sun');
+        }else{
+          element.classList.remove('la-sun');
+          element.classList.add('la-star-and-crescent');
+        };
+      });
+      logoicon.forEach(element => {
+        element.classList.toggle('light');
+        if(element.classList.contains('light')){
+          element.src = "assets/images/akag-g-only-light.png";
+        }else{
+          element.src = "assets/images/akag-g-only.png";
+        };
+      });
+  });
+});
